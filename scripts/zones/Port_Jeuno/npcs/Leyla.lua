7111e58e4bc1a94c27bae6e3432fa5945bfb95b3
Fleme <sylvain.santucci@gmail.com>
2022-12-05 12:19:52 +0000

    update npc

--- scripts/zones/Port_Jeuno/npcs/Leyla.lua
+++ scripts/zones/Port_Jeuno/npcs/Leyla.lua
@@ -14,19 +14,19 @@ end
 entity.onTrigger = function(player, npc)
     local stock =
     {
-        17308,  55,    -- Hawkeye
-        17320,   7,    -- Iron Arrow
-        17336,   5,    -- Crossbow Bolt
-        4509,   10,    -- Distilled Water
-        5038, 1000,    -- Enchanting Etude
-        5037, 1265,    -- Spirited Etude
-        5036, 1567,    -- Learned Etude
-        5035, 1913,    -- Quick Etude
-        5034, 2208,    -- Vivacious Etude
-        5033, 2815,    -- Dextrous Etude
-        5032, 3146,    -- Sinewy Etude
+        -- 17308,  55,    -- Hawkeye
+        -- 17320,   7,    -- Iron Arrow
+        -- 17336,   5,    -- Crossbow Bolt
+        -- 4509,   10,    -- Distilled Water
+        -- 5038, 1000,    -- Enchanting Etude
+        -- 5037, 1265,    -- Spirited Etude
+        -- 5036, 1567,    -- Learned Etude
+        -- 5035, 1913,    -- Quick Etude
+        -- 5034, 2208,    -- Vivacious Etude
+        -- 5033, 2815,    -- Dextrous Etude
+        -- 5032, 3146,    -- Sinewy Etude
         1126, 10000,    -- BS
-        1127, 10000,    -- KS
+        1127, 15000,    -- KS
     }
 
     player:showText(npc, ID.text.DUTY_FREE_SHOP_DIALOG)
